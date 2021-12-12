74bf326d9a0376faecb5423996ae4425b861fb89
Nbiba Bedis <bedisnbiba@gmail.com>
2021-12-12 09:59:29 +0100

    Improvements

--- src/kmap.rs
+++ src/kmap.rs
@@ -1,8 +1,10 @@
 use crate::kvec::KVec;
 
-/// HashMap like struct
+/// Map like struct
 ///
-/// Inorder to use it you need to wrap it in a custom type
+/// This is an ordered Map, that's why it provides functions like `get_by_idx`
+///
+/// In-order to use it you need to wrap it in a custom type
 ///
 /// ```rust
 /// use konster::kvec::KVec;
@@ -26,9 +28,10 @@ use crate::kvec::KVec;
 /// impl Map {
 ///     const fn get(&self, key: &u8) -> Option<usize> {
 ///         let mut idx = 0;
-///         while idx < self.map.vec.len() {
-///             if self.map.vec.get_unchecked(idx).0 == *key {
-///                 return Some(self.map.vec.buf[idx].1);
+///         while idx < self.map.len() {
+///             let elem = self.map.vec.get_unchecked(idx);
+///             if elem.0 == *key {
+///                 return Some(elem.1);
 ///             }
 ///             idx += 1;
 ///         }
@@ -36,7 +39,7 @@ use crate::kvec::KVec;
 ///     }
 ///     const fn insert(mut self, key: u8, value: usize) -> Self {
 ///         let mut idx = 0;
-///         while idx < self.map.vec.len() {
+///         while idx < self.map.len() {
 ///             if self.map.vec.get_unchecked(idx).0 == key {
 ///                 self.map.vec.buf[idx].1 = value;
 ///                 return self;
@@ -52,4 +55,26 @@ use crate::kvec::KVec;
 pub struct KMap<K, V, const N: usize> {
     /// The backing Vector of the Map
     pub vec: KVec<(K, V), N>,
+}
+
+impl<K, V, const N: usize> KMap<K, V, N> {
+    /// Returns the number of elements in the Map, also referred to as its ‘length’.
+    pub const fn len(&self) -> usize {
+        self.vec.len()
+    }
+    /// Returns true if the Map contains no elements.
+    pub const fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+    /// Returns the element at the index, or None if its empty.
+    pub const fn get_by_idx(&self, index: usize) -> Option<&V> {
+        match self.vec.get(index) {
+            Some((_k, v)) => Some(v),
+            None => None,
+        }
+    }
+    /// Returns the element at the index, with no bounds check
+    pub const fn get_by_idx_unchecked(&self, index: usize) -> &V {
+        &self.vec.get_unchecked(index).1
+    }
 }
